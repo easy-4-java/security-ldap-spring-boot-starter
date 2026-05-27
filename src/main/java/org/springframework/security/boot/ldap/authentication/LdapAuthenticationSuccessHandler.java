260eb8f0ded86c73f570d8423e920f65dade4a72
wandl <hiwepy@163.com>
2026-05-27 11:10:18 +0800

    chore(pom): align to Spring Boot 4.1.x (JDK 17, Boot 4.1.0-M4)

--- src/main/java/org/springframework/security/boot/ldap/authentication/LdapAuthenticationSuccessHandler.java
+++ src/main/java/org/springframework/security/boot/ldap/authentication/LdapAuthenticationSuccessHandler.java
@@ -6,9 +6,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
