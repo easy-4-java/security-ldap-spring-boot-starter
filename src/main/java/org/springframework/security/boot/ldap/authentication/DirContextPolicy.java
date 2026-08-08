fd1e96f710bc0e90c0b93fbb9ad1f41271ffa052
wandl-6A72h <hiwepy@gmail.com>
2026-08-09 00:04:34 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 3.2.x branch

--- src/main/java/org/springframework/security/boot/ldap/authentication/DirContextPolicy.java
+++ src/main/java/org/springframework/security/boot/ldap/authentication/DirContextPolicy.java
@@ -20,7 +20,7 @@ import org.springframework.ldap.core.support.DirContextAuthenticationStrategy;
 
 /**
  * Dir Context Strategy
- * @author ： <a href="https://github.com/hiwepy">wandl</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public enum DirContextPolicy {
 
