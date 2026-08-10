41beab42aa6954e3ba234c4aa2c1109434d6ec01
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:58:27 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/springframework/security/boot/ldap/authentication/DirContextPolicy.java
+++ src/main/java/org/springframework/security/boot/ldap/authentication/DirContextPolicy.java
@@ -20,7 +20,7 @@ import org.springframework.ldap.core.support.DirContextAuthenticationStrategy;
 
 /**
  * Dir Context Strategy
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 public enum DirContextPolicy {
 
