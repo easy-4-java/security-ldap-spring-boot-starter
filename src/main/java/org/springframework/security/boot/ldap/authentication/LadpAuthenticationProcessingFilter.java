24773b7681f83313fbc7cbdee2b77116d4e832c8
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:58:23 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/springframework/security/boot/ldap/authentication/LadpAuthenticationProcessingFilter.java
+++ src/main/java/org/springframework/security/boot/ldap/authentication/LadpAuthenticationProcessingFilter.java
@@ -26,7 +26,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * TODO
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 public class LadpAuthenticationProcessingFilter extends PostRequestAuthenticationProcessingFilter {
 
